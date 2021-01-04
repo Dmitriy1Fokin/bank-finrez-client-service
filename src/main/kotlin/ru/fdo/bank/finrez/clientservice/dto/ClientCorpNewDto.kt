54cbfe12643a0d10121170c209cb209180c808e7
Dmitriy Fokin <dmitrii.fokin@aisa.ru>
2021-01-05 00:40:50 +0300

    added ClientCorpNewDto and TypeOfClient

---
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservice/dto/ClientCorpNewDto.kt
@@ -0,0 +1,19 @@
+package ru.fdo.bank.finrez.clientservice.dto
+
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+
+data class ClientCorpNewDto(@field:NotNull val typeOfClient: TypeOfClient,
+                            @field:NotBlank val name: String,
+                            @field:NotBlank val orgForm: String,
+                            @field:NotBlank val abbreviatedName: String,
+                            @field:Pattern(regexp = "[0-9]{10}", message = "Incorrect value") val inn: String,
+                            @field:Pattern(regexp = "[0-9]{9}", message = "Incorrect value") val kpp: String,
+                            @field:Pattern(regexp = "[0-9]{13}", message = "Incorrect value") val ogrn: String,
+                            @field:Pattern(regexp = "[0-9]{10}", message = "Incorrect value") val okpo: String,
+                            @field:Pattern(regexp = "[0-9]{2}\\.[0-9]{2}\\.[0-9]{2}", message = "Incorrect value") val okved: String,
+                            @field:NotBlank val addressU: String,
+                            @field:NotBlank val addressF: String,
+                            @field:Email val email: String)
---
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservice/dto/TypeOfClient.kt
@@ -0,0 +1,5 @@
+package ru.fdo.bank.finrez.clientservice.dto
+
+enum class TypeOfClient {
+    CORP, INDIVIDUAL
+}
