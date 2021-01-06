87a2cd97f46ca689c32a5787783bc18920e9d95a
Dmitriy Fokin <dmitrii.fokin@aisa.ru>
2021-01-06 23:28:11 +0300

    added updateClientCorp method

--- src/main/kotlin/ru/fdo/bank/finrez/clientservice/controller/ClientController.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservice/controller/ClientController.kt
@@ -1,10 +1,12 @@
 package ru.fdo.bank.finrez.clientservice.controller
 
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.fdo.bank.finrez.clientservice.dto.ClientCorpNewDto
+import ru.fdo.bank.finrez.clientservice.dto.ClientCorpUpdateDto
 import ru.fdo.bank.finrez.clientservice.service.ClientService
 import java.util.UUID
 import java.util.concurrent.CompletableFuture
@@ -17,6 +19,10 @@ import javax.validation.Valid
 class ClientController(private val clientService: ClientService) {
 
     @PostMapping("/create")
-    fun createClient(@Valid @RequestBody clientCorpNewDto : ClientCorpNewDto) : CompletableFuture<UUID> =
+    fun createClientCorp(@Valid @RequestBody clientCorpNewDto : ClientCorpNewDto) : CompletableFuture<UUID> =
         clientService.createClientCorp(clientCorpNewDto)
+
+    @PutMapping("/update")
+    fun updateClientCorp(@Valid @RequestBody clientCorpUpdateDto: ClientCorpUpdateDto) : CompletableFuture<UUID> =
+            clientService.updateClientCorp(clientCorpUpdateDto)
 }
