1e2941ec6d15d8c5f5f83b1802753513cd010867
Dmitriy Fokin <dmitrii.fokin@aisa.ru>
2021-01-06 23:27:48 +0300

    added updateClientCorp method

--- src/main/kotlin/ru/fdo/bank/finrez/clientservice/service/ClientService.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservice/service/ClientService.kt
@@ -1,9 +1,11 @@
 package ru.fdo.bank.finrez.clientservice.service
 
 import ru.fdo.bank.finrez.clientservice.dto.ClientCorpNewDto
+import ru.fdo.bank.finrez.clientservice.dto.ClientCorpUpdateDto
 import java.util.*
 import java.util.concurrent.CompletableFuture
 
 interface ClientService {
     fun createClientCorp(clientCorpNewDto: ClientCorpNewDto): CompletableFuture<UUID>
+    fun updateClientCorp(clientCorpUpdateDto: ClientCorpUpdateDto): CompletableFuture<UUID>
 }
--- src/main/kotlin/ru/fdo/bank/finrez/clientservice/service/impl/ClientServiceImpl.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservice/service/impl/ClientServiceImpl.kt
@@ -3,7 +3,9 @@ package ru.fdo.bank.finrez.clientservice.service.impl
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.springframework.stereotype.Service
 import ru.fdo.bank.finrez.clientservice.coreapi.command.CreateClientCorpCommand
+import ru.fdo.bank.finrez.clientservice.coreapi.command.UpdateClientCorpCommand
 import ru.fdo.bank.finrez.clientservice.dto.ClientCorpNewDto
+import ru.fdo.bank.finrez.clientservice.dto.ClientCorpUpdateDto
 import ru.fdo.bank.finrez.clientservice.service.ClientService
 import java.util.UUID
 import java.util.concurrent.CompletableFuture
@@ -12,6 +14,9 @@ import java.util.concurrent.CompletableFuture
 class ClientServiceImpl(private val commandGateway: CommandGateway) : ClientService {
 
     override fun createClientCorp(clientCorpNewDto: ClientCorpNewDto): CompletableFuture<UUID> =
-        commandGateway.send(CreateClientCorpCommand(UUID.randomUUID(), clientCorpNewDto))
+        commandGateway.send(CreateClientCorpCommand(UUID.randomUUID().toString(), clientCorpNewDto))
+
+    override fun updateClientCorp(clientCorpUpdateDto: ClientCorpUpdateDto): CompletableFuture<UUID> =
+        commandGateway.send(UpdateClientCorpCommand(clientCorpUpdateDto.clientId, clientCorpUpdateDto))
 
 }
