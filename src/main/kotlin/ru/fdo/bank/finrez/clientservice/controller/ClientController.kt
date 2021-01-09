93a1fab6024ac7f91a08271fe7bdfb9cb3874707
Dmitriy Fokin <fokinds1@gmail.com>
2021-01-10 00:15:24 +0300

    added new post-put request individual client

--- src/main/kotlin/ru/fdo/bank/finrez/clientservice/controller/ClientController.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/clientservice/controller/ClientController.kt
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.fdo.bank.finrez.clientservice.dto.ClientCorpNewDto
 import ru.fdo.bank.finrez.clientservice.dto.ClientCorpUpdateDto
+import ru.fdo.bank.finrez.clientservice.dto.ClientIndividualNewDto
+import ru.fdo.bank.finrez.clientservice.dto.ClientIndividualUpdateDto
 import ru.fdo.bank.finrez.clientservice.service.ClientService
 import java.util.UUID
 import java.util.concurrent.CompletableFuture
@@ -18,11 +20,19 @@ import javax.validation.Valid
 @RequestMapping("/client/cmd")
 class ClientController(private val clientService: ClientService) {
 
-    @PostMapping("/create")
+    @PostMapping("/create/corp")
     fun createClientCorp(@Valid @RequestBody clientCorpNewDto : ClientCorpNewDto) : CompletableFuture<UUID> =
         clientService.createClientCorp(clientCorpNewDto)
 
-    @PutMapping("/update")
+    @PutMapping("/update/corp")
     fun updateClientCorp(@Valid @RequestBody clientCorpUpdateDto: ClientCorpUpdateDto) : CompletableFuture<UUID> =
             clientService.updateClientCorp(clientCorpUpdateDto)
+
+    @PostMapping("/create/ind")
+    fun createClientIndividual(@Valid @RequestBody clientIndividualNewDto : ClientIndividualNewDto) : CompletableFuture<UUID> =
+            clientService.createClientIndividual(clientIndividualNewDto)
+
+    @PutMapping("/update/ind")
+    fun updateClientIndividual(@Valid @RequestBody clientIndividualUpdateDto: ClientIndividualUpdateDto) : CompletableFuture<UUID> =
+            clientService.updateClientIndividual(clientIndividualUpdateDto)
 }
